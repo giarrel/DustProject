5e35556df063d6c3bd341e5e4c78686ab65d54c7
giarrel <lucacag@outlook.de>
2023-02-28 11:31:04 +0100

    only gravity yet

--- dust.jl
+++ dust.jl
@@ -1,6 +1,6 @@
 using DifferentialEquations
 using Plots
-#blba
+
 
 const GM = 1.327e20::Float64                            # G * M_sol [m^3 / s^2]
 const AU = 1.496e11::Float64                            # 1AU [m]
